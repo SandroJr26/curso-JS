e6a6bbe0e2de22c20bed1d163a8800f3730c2986
Sandro Junior <nunet26@gmail.com>
2023-02-06 22:44:42 -0300

    js

--- animacao-ao-scroll/script.js
+++ animacao-ao-scroll/script.js
@@ -52,7 +52,7 @@ function initScrollSuave() {
 
     window.scrollTo({
       top: topo,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
 
     // Forma alternativa 1
@@ -75,3 +75,25 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
