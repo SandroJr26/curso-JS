2590151ea3ff89c869b72f1ac7411a67cdfb0f96
Sandro Junior <nunet26@gmail.com>
2023-04-06 03:52:01 -0300

    module-introducao

---
+++ efeitos-no-dom/module-introducao/js/modules/teste.js
@@ -0,0 +1,11 @@
+export function teste1() {
+  console.log("Isso é um teste 1");
+}
+
+export function teste2() {
+  console.log("Isso é um teste 2");
+}
+
+var esseName = "Sandro";
+
+export const senha = 1234123;
---
+++ efeitos-no-dom/module-introducao/js/script.js
@@ -0,0 +1,18 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+// import { teste1, teste2 } from "./modules/teste.js";
+import * as teste from "./modules/teste.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+
+// teste1();
+// teste2();
+
+teste.teste1();
+teste.teste2();
+console.log(teste.senha);
