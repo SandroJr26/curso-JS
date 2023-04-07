4d2ede51e3532efa17d0b6d822b400cdfcde3a71
Sandro Junior <nunet26@gmail.com>
2023-04-07 18:18:50 -0300

    init tooltip

---
+++ efeitos-no-dom/tooltip/js/modules/tooltip.js
@@ -0,0 +1,23 @@
+export default function initTooltip() {}
+
+const tooltips = document.querySelectorAll("[data-tooltip]");
+
+function onMouseOver(event) {
+  const tooltipBox = criarTooltipBox(this);
+
+  console.log(event);
+}
+
+tooltips.forEach((item) => {
+  item.addEventListener("mouseover", onMouseOver);
+});
+
+function criarTooltipBox(element) {
+  const tooltipBox = document.createElement("div");
+  const text = element.getAttribute("aria-label");
+
+  tooltipBox.classList.add("tooltip");
+  tooltipBox.innerText = text;
+  document.body.appendChild(tooltipBox);
+  return tooltipBox;
+}
